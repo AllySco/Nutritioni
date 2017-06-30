8127b918974726e15ec6ee9e5ebbf7cb3d771b18
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-06-30 16:01:57 +0100

    server setup git push

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("client/build"));
+// app.use(require('./controllers/index.js'))
+app.listen( 3001, function(){console.log("fuckin' yaldi")});
