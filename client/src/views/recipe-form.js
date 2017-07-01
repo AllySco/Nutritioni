e738a32f88c3943a91bd8a145bc11dd2e5856fd6
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-07-01 18:36:16 +0100

    Revert "revert commit to split it into 2"
    This reverts commit 9aa1393a2dc8b8e4d4e63f1c277bec7303a5cff1.

--- client/src/views/recipe-form.js
+++ client/src/views/recipe-form.js
@@ -1,36 +1,21 @@
-var RecipeRequest = require('../edamam_api/recipe-request.js');
-
 var RecipeForm = function() {
   var main = document.querySelector('main');
-  this.form = this.createForm();
-  main.appendChild(this.form);
-  this.form.addEventListener('submit', this.handleSubmit.bind(this));
+  var form = document.createElement('form');
+  var submit = document.createElement('input');
+  submit.type = 'submit';
+  submit.value = 'Analyse Recipe';
+  form.id = 'recipe-form';
+  var title = this.createInput('title');
+  var ingredients = this.createInput('ingredients');
+
+
+  main.appendChild(form);
+  form.appendChild(title);
+  form.appendChild(ingredients);
+  form.appendChild(submit);
 }
 
 RecipeForm.prototype = {
-  handleSubmit: function(event) {
-    event.preventDefault();
-    var data = {
-      title: this.form['title'].value,
-      ingr: [this.form['ingredients'].value]
-    }
-    var request = new RecipeRequest();
-    request.makePostRequest(data);
-  },
-  createForm: function() {
-    var form = document.createElement('form');
-    form.id = 'recipe-form';
-    form.method = 'post';
-    form.action = '/';
-    var title = this.createInput('title');
-    var ingredients = this.createInput('ingredients');
-    var submit = this.createSubmitButton();
-
-    form.appendChild(title);
-    form.appendChild(ingredients);
-    form.appendChild(submit);
-    return form;
-  },
   createInput: function(name) {
     var container = document.createElement('div');
     var label = document.createElement('label');
@@ -43,12 +28,6 @@ RecipeForm.prototype = {
     container.appendChild(label);
     container.appendChild(input);
     return container;
-  },
-  createSubmitButton: function() {
-    var submit = document.createElement('input');
-    submit.type = 'submit';
-    submit.value = 'Analyse Recipe';
-    return submit;
   }
 }
 
