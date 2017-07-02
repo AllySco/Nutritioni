bd4ebc75785669c46e286c48f3ae65e80cb756c7
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-02 19:29:19 +0100

    test file created

---
+++ client/src/models/recipe_specs.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var Athlete = require('../recipe.js');
+
+describe("Recipe", function() {
+
+
+  var recipe;
+
+  beforeEach(function() {
+    recipe = new Recipe();
+  })
+
+
+  })
+
+
