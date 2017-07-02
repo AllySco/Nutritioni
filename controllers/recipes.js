96b1103b95c2c5091b9976e50f01f02fa869fe2c
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-02 19:17:25 +0100

    working NEW route added

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,11 +5,26 @@ var recipeRouter = express.Router();
 var RecipeQuery = require('../db/recipeQuery.js');
 var query = new RecipeQuery();
 
+// index
 recipeRouter.get('/', function(req, res) {
 	query.all(function(recipes) {
 		res.json(recipes);
 	});
 });
+
+// NEW
+recipeRouter.post('/', function(req, res) {
+  var newRecipe = { 
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    nutritionalInformation: req.body.nutritionalInformation
+  }
+  query.add( newRecipe, function(allRecipes) {
+    res.json(allRecipes);
+  })
+})
+
+
 
 
 module.exports = recipeRouter;
