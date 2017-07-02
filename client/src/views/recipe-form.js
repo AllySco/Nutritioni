f58aef79d56fa968c8e9ea7ad8ccdb8318745461
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-07-02 15:04:54 +0100

    add the ability to add multiple ingredients to recipe

--- client/src/views/recipe-form.js
+++ client/src/views/recipe-form.js
@@ -10,12 +10,25 @@ var RecipeForm = function() {
 RecipeForm.prototype = {
   handleSubmit: function(event) {
     event.preventDefault();
+    var ingredientInputs = document.querySelectorAll('.ingredients input');
+    var ingredients = [];
+    for (var i = 0; i < ingredientInputs.length; i++) {
+      var ingredient = ingredientInputs[i].value;
+      if (ingredient !== "") ingredients.push(ingredient);
+    }
+    console.log("ingredients mapped", ingredients);
     var data = {
       title: this.form['title'].value,
-      ingr: [this.form['ingredients'].value]
+      ingr: ingredients
     }
     var request = new RecipeRequest();
     request.makePostRequest(data);
+    var inputs = document.querySelectorAll('input[type=text]');
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = "";
+    }
+
+    console.log(inputs);
   },
   createForm: function() {
     var form = document.createElement('form');
@@ -25,6 +38,7 @@ RecipeForm.prototype = {
     var title = this.createInput('title');
     var ingredients = this.createInput('ingredients');
     var submit = this.createSubmitButton();
+    this.addIngredientButton(ingredients);
 
     form.appendChild(title);
     form.appendChild(ingredients);
@@ -33,6 +47,7 @@ RecipeForm.prototype = {
   },
   createInput: function(name) {
     var container = document.createElement('div');
+    container.classList.add(name);
     var label = document.createElement('label');
     label.setAttribute('for', name);
     label.innerText = name.replace(/\b\w/g, function(l) {
@@ -40,6 +55,7 @@ RecipeForm.prototype = {
     });
     var input = document.createElement('input');
     input.id = name;
+    input.type = "text";
     container.appendChild(label);
     container.appendChild(input);
     return container;
@@ -49,6 +65,19 @@ RecipeForm.prototype = {
     submit.type = 'submit';
     submit.value = 'Analyse Recipe';
     return submit;
+  },
+  addIngredientButton: function(container) {
+    var plusButton = document.createElement('button');
+    plusButton.innerText = '+';
+    plusButton.type = "button";
+    container.appendChild(plusButton);
+    plusButton.addEventListener('click', this.handleAddIngredientClick);
+  },
+  handleAddIngredientClick: function() {
+    var input = document.createElement('input');
+    input.type = "text"
+    var container = document.querySelector('.ingredients');
+    container.insertBefore(input, container.children[container.children.length -1]);
   }
 }
 
