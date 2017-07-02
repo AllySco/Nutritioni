8d9d059aed5e866866d680e135495b1b707b1cd8
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-02 19:21:43 +0100

    created recipe model

---
+++ client/src/models/Recipe.js
@@ -0,0 +1,7 @@
+var Recipe = function(options) {
+  this.title= options.title;
+  this.ingredients = options.ingredients;
+  this.nutritionalInformation = options.nutritionalInformation;
+}
+
+module.exports = Recipe;
