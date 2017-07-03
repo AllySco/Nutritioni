79aa3203275f9c0802d86cbe57a7afeed7a7b54d
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-07-03 23:43:04 +0100

    rename file for request to edamam

--- client/src/apis/recipe-analysis-request.js
+++ client/src/apis/recipe-analysis-request.js
@@ -19,34 +19,39 @@ RecipeRequest.prototype = {
 		if (this.request.status !== 200) return;
 		var jsonResponse = this.request.responseText;
 		var responseData = JSON.parse(jsonResponse);
-		console.log("response data line 23", responseData)
 
 		var nutrients = responseData.totalNutrients;
-		console.log("nutrients line 26", nutrients)
 		var NutrientData = [{ 
-				name: "vitamins",
-				data: [
-			{y: nutrients.CA.quantity , color: 'red'},
-			{y: nutrients.FE.quantity, color: 'blue'},
-			{y: nutrients.K.quantity, color: 'orange'},
-			{y: nutrients.MG.quantity, color: 'purple'},
-			{y: nutrients.P.quantity, color: 'brown'},
-			{y: nutrients.TOCPHA.quantity, color: 'yellow'},
-			{y: nutrients.ZN.quantity, color: 'black'},
+			name: "vitamins",
+			data: [
+				{ y: nutrients.CA.quantity , color: 'red' },
+				{ y: nutrients.FE.quantity, color: 'blue' },
+				{ y: nutrients.K.quantity, color: 'orange' },
+				{ y: nutrients.MG.quantity, color: 'purple' },
+				{ y: nutrients.P.quantity, color: 'brown' },
+				{ y: nutrients.TOCPHA.quantity, color: 'yellow' },
+				{ y: nutrients.ZN.quantity, color: 'black' },
 			]
 		}];
-			console.log("line 36", NutrientData)
 
-			var NutrientLabels = [ "Calcium", 
-			"Iron", "Potassium", "Magnesium", "Potassium", "Vitamin E", "Zinc"]
-
-
-		console.log("line 47", NutrientLabels);		
+		var NutrientLabels = [ "Calcium", "Iron", "Potassium", "Magnesium", "Phosphorous", "Vitamin E", "Zinc"];
 
 		var pieChartData = [
-			{ name: nutrients.CHOCDF.label, y: nutrients.CHOCDF.quantity, color: 'red' },
-			{ name: nutrients.FAT.label, y: nutrients.FAT.quantity, color: 'green' },
-			{ name: nutrients.PROCNT.label, y: nutrients.PROCNT.quantity, color: 'blue' }
+			{ 
+				name: nutrients.CHOCDF.label,
+				y: nutrients.CHOCDF.quantity,
+				color: 'red'
+			},
+			{ 
+				name: nutrients.FAT.label,
+				y: nutrients.FAT.quantity,
+				color: 'green'
+			},
+			{ 
+				name: nutrients.PROCNT.label,
+				y: nutrients.PROCNT.quantity,
+				color: 'blue'
+			}
 		];
 
 
