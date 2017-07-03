b6fe6adaccffe0cf4a01ed592a5262773f84a59c
Charlie Wood <charliemowood@gmail.com>
2017-07-03 15:16:22 +0100

    Google map stuff not working yet

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,10 +1,26 @@
 var Canvas = require('./canvas.js');
 var RecipeForm = require('./recipe-form.js');
+var MapWrapper = require('./map-wrapper.js')
 
 var UI = function() {
 
 	// new Canvas();
 	new RecipeForm();
+
+
+var initialize = function() {
+  var main = document.querySelector('main');
+	var container = document.createElement('div');
+
+  var cords = {lat:51, lng: 5}
+
+  var zoom = 8
+  var mainMap = new MapWrapper(location, cords, zoom);
+  mainMap.addMarker(cords);
+  mainMap.addClickEvent();
+  main.appendChild(container);
+}
+window.addEventListener('load', initialize);
 }
 
 UI.prototype = {
