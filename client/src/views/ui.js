a12078eb5f4e80bd8f36b8fde33b55c724dd7f02
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-07-04 14:32:21 +0100

    refactor recipe analyser

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -12,9 +12,9 @@ var UI = function() {
 UI.prototype = {
 	render: function() {
 		// new Navigation();
-		 // new RecipeAnalyser();
+		 new RecipeAnalyser();
 		// new LandingPage();
-		new StoreLocator();
+		// new StoreLocator();
 	}
 }
 
