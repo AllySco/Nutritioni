58f79c41871e996f0c6a4427d7cd7cbcaffe8150
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-07-04 22:21:09 +0100

    assigning values on column-chart object

--- client/src/charts/column-chart.js
+++ client/src/charts/column-chart.js
@@ -15,12 +15,19 @@ var ColumnChart = function(title, data, categories) {
     },
     series: data,
     xAxis: {
+      gridLineColor: '#292828',
+      className: 'column-chart-x-axis',
       categories: categories
     },
     legend: {
       enabled: false
     },
     yAxis: {
+            gridLineColor: '#696969',
+            className: 'column-chart-y-axis',
+            title: {
+              text: 'Mass mg'
+            },
             labels: {
                 format: '{value} mg'
             }
