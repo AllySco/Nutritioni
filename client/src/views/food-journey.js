85cb7d0533cb2d02929bb564c4525ebc54298eed
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-05 12:41:18 +0100

    basic functional mockup working

--- client/src/views/food-journey.js
+++ client/src/views/food-journey.js
@@ -10,24 +10,48 @@ FoodJourney.prototype = {
 render: function() {
   var main = document.createElement('main');
   main.id = "food-journey";
-  var infoContainer = document.createElement('div')
-  infoContainer.id = 'info-container';
 
-  var infoHeader = document.createElement('div')
-  infoHeader.id = "info-header"
+  var infoContainer = this.createInfoContainer();
 
-  var h2 = document.createElement('h2');
-  h2.innerText = "YOUR BODY AND FOOD";
+  var infoHeader = this.createInfoHeader();
 
-  var opacityWrapper = document.createElement('div');
-  opacityWrapper.id = 'opacity-wrapper'
+  var h2 = this.createTitle();
 
+  var opacityWrapper = this.createOpacityWrapper();
 
+  infoHeader.appendChild(h2)
   infoContainer.appendChild(infoHeader)
+  infoContainer.appendChild(opacityWrapper)
   main.appendChild(infoContainer)
   document.body.appendChild(main)
 
-}
+},
+
+createInfoContainer: function() {
+  var infoContainer = document.createElement('div')
+  infoContainer.id = 'info-container';
+  return infoContainer;
+},
+
+createInfoHeader: function() {
+  var infoHeader = document.createElement('div')
+  infoHeader.id = "info-header"
+  return infoHeader;
+
+},
+
+createTitle: function() {
+  var h2 = document.createElement('h2');
+  h2.innerText = "YOUR BODY AND FOOD";
+  return h2;
+},
+
+createOpacityWrapper: function() {
+  var opacityWrapper = document.createElement('div');
+  opacityWrapper.id = 'opacity-wrapper'
+  return opacityWrapper
+},
+
 }
 
 module.exports = FoodJourney;
